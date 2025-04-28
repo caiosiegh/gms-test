93474fcfd636db80a356495c3bd4914b56e95f4a
Caio Gusmão <caio.nove@hotmail.com>
2025-04-28 14:46:25 -0300

    ci-cd

--- cypress/e2e/Filmes.cy.js
+++ cypress/e2e/Filmes.cy.js
@@ -1,22 +1,29 @@
 /// <reference types="cypress" />
 
 describe('Funcionalidade: Filmes', () => {
+
+  beforeEach(() => {
+    cy.visit('')
+  })
+
+  afterEach(() => {
+    cy.screenshot()
+  })
+
+
     it('Buscar Filme com palavra-chave válida', () => {
-      cy.visit('http://192.168.100.5:8080')
       cy.get('#search-input').type('Jurassic')
       cy.get('#search-button').click()
       cy.get('#results-section').should('contain.text', "Jurassic")
     })
 
     it('Buscar Filme sem resultado', () => {
-        cy.visit('http://192.168.100.5:8080')
         cy.get('#search-input').type('hsjfhayu')
         cy.get('#search-button').click()
         cy.get('#results-section').should('have.text', 'Filme não encontrado.')
       })
 
       it('Funcionalidade Limpar Buscar', () => {
-        cy.visit('http://192.168.100.5:8080')
         cy.get('#search-input').type('Jurassic')
         cy.get('#search-button').click()
         cy.get('#results-section').should('be.visible')
--- cypress/e2e/Login.cy.js
+++ cypress/e2e/Login.cy.js
@@ -8,13 +8,16 @@ describe('Funcionalidade: Cadastro de Membros', () => {
     cy.visit('/')
   })
 
+  afterEach(() => {
+    cy.screenshot()
+  })
+
   it('Deve fazer o cadastro de campos Obrigatórios', () => {
     cy.preencherCadastro('Caio', 'Testando', email, '31998565478', 'Teste123!')
     cy.get('#signup-response').should('be.visible')
   })
 
   it('Validar formato do e-mail', () => {
-    cy.visit('http://192.168.100.5:8080')
     cy.get('#signup-firstname').type('Caio')
     cy.get('#signup-lastname').type('Testando')
     cy.get('#signup-email').type('applicationTest1gmail.com')
@@ -25,13 +28,11 @@ describe('Funcionalidade: Cadastro de Membros', () => {
   })
 
   it('Envio sem campos Obrigatórios', () => {
-    cy.visit('http://192.168.100.5:8080')
     cy.get('#signup-button').click()
     cy.get('#signup-response').should('have.text', '{"message":"Nome não pode estar vazio"}')
   })
 
   it('Testando e-mail válido', () => {
-    cy.visit('http://192.168.100.5:8080')
     cy.get('#signup-firstname').type('Caio')
     cy.get('#signup-lastname').type('Testando')
     cy.get('#signup-email').type('applicationTest1gmail.com')
@@ -42,7 +43,6 @@ describe('Funcionalidade: Cadastro de Membros', () => {
   })
 
   it('E-mail já existente', () => {
-    cy.visit('http://192.168.100.5:8080')
     cy.get('#signup-firstname').type('Caio')
     cy.get('#signup-lastname').type('Testando')
     cy.get('#signup-email').type('applicationTest11@gmail.com')
@@ -53,7 +53,6 @@ describe('Funcionalidade: Cadastro de Membros', () => {
   })
 
   it('Testando a senha fraca sem Maiúscula', () => {
-    cy.visit('http://192.168.100.5:8080')
     cy.get('#signup-firstname').type('Caio')
     cy.get('#signup-lastname').type('Testando')
     cy.get('#signup-email').type('applicationTes@gmail.com')
@@ -64,7 +63,6 @@ describe('Funcionalidade: Cadastro de Membros', () => {
   })
 
   it('Testando a senha fraca', () => {
-    cy.visit('http://192.168.100.5:8080')
     cy.get('#signup-firstname').type('Caio')
     cy.get('#signup-lastname').type('Testando')
     cy.get('#signup-email').type('applicationTes@gmail.com')
@@ -75,13 +73,12 @@ describe('Funcionalidade: Cadastro de Membros', () => {
   })
 
   it('Testando a Política de Privacidade', () => {
-    cy.visit('http://192.168.100.5:8080')
-    cy.get('a[href="./polices.html"]').click()
-    cy.get('h1').should('have.text', 'Política de Privacidade')
+    cy.get('a').click()
+    cy.wait(4000)
+    cy.title().should('have.text', 'Política de Privacidade - Golden Movie Studio')
   })
 
   it('Senha vazia', () => {
-    cy.visit('http://192.168.100.5:8080')
     cy.get('#signup-firstname').type('Caio')
     cy.get('#signup-lastname').type('Testando')
     cy.get('#signup-email').type('applicationTes@gmail.com')
